6aca7f553ff5b9e41fe33a22a313d4ff63942574
PattyFrei <patty.github@pm.me>
2020-10-20 21:49:37 +0200

    add correct types for http requests

--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { Character } from './models/character';
+import { Character, People } from './models/character';
 
 @Injectable({
     providedIn: 'root',
@@ -11,11 +11,11 @@ export class SwapiService {
     swapiUrl = 'https://swapi.dev/api';
     constructor(private http: HttpClient) {}
 
-    getPeople() {
-        return this.http.get<any>(`${this.swapiUrl}/people`);
+    getPeople(): Observable<People> {
+        return this.http.get<People>(`${this.swapiUrl}/people`);
     }
 
-    getCharacter(id: string): Observable<Character> {
-        return this.http.get<Character>(`${this.swapiUrl}/people/${id}`);
+    getCharacter(id: number): Observable<Character> {
+        return this.http.get<Character>(`${this.swapiUrl}/people/${id}/`);
     }
 }
