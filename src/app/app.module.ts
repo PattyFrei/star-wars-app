75b1787f93a5465822b7f550f5d03315eed1f37c
PattyFrei <patty.github@pm.me>
2020-10-21 09:44:13 +0200

    create character detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PeopleComponent } from './people/people.component';
+import { CharacterDetailComponent } from './character-detail/character-detail.component';
 
 @NgModule({
-    declarations: [AppComponent, PeopleComponent],
+    declarations: [AppComponent, PeopleComponent, CharacterDetailComponent],
     imports: [BrowserModule, AppRoutingModule, HttpClientModule],
     providers: [],
     bootstrap: [AppComponent],
