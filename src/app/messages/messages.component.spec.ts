24f41f561771545548a16f9f18d1a86ec5a2393f
PattyFrei <patty.github@pm.me>
2020-11-12 15:02:09 +0100

    add unit tests

--- src/app/messages/messages.component.spec.ts
+++ src/app/messages/messages.component.spec.ts
@@ -3,23 +3,31 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MessagesComponent } from './messages.component';
 
 describe('MessagesComponent', () => {
-  let component: MessagesComponent;
-  let fixture: ComponentFixture<MessagesComponent>;
+    let component: MessagesComponent;
+    let fixture: ComponentFixture<MessagesComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ MessagesComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [MessagesComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MessagesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(MessagesComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should render default message', () => {
+        fixture = TestBed.createComponent(MessagesComponent);
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        expect(
+            compiled.querySelector('.system .system-logs div').textContent
+        ).toContain('user @ star terminal:');
+    });
 });
